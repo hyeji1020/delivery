379422ab23e3d2e51f424a0c3d3f9db8e1cbe37c
hyeji <hyeji6742@naver.com>
2023-11-27 14:48:20 +0900

    FoodRepository 인터페이스 추가

---
+++ src/main/java/com/study/sjicnho/delivery/food/repository/FoodRepository.java
@@ -0,0 +1,10 @@
+package com.study.sjicnho.delivery.food.repository;
+
+import com.study.sjicnho.delivery.food.domain.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FoodRepository extends JpaRepository<Food, Integer> {
+
+}
