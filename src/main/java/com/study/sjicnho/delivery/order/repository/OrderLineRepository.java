19cb4e1e1fc29ac54c72aad5cca88a450aa66c5a
hyeji <hyeji6742@naver.com>
2024-02-06 19:40:40 +0900

    refactor:누락된 어노테이션 추가(@Repository)

--- src/main/java/com/study/sjicnho/delivery/order/repository/OrderLineRepository.java
+++ src/main/java/com/study/sjicnho/delivery/order/repository/OrderLineRepository.java
@@ -2,9 +2,10 @@ package com.study.sjicnho.delivery.order.repository;
 
 import com.study.sjicnho.delivery.order.entity.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
-
 }
