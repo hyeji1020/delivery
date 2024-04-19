dd5726085392c9df3d1537239e041b51258ea135
hyeji <hyeji6742@naver.com>
2024-04-19 17:07:27 +0900

    나머지 파일 commit

--- src/main/java/com/study/sjicnho/delivery/user/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.study.sjicnho.delivery.user;
-
-public enum RoleType {
-
-    ADMIN, USER
-}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into store
+values (1, '서울시 금천구 시흥동 123', '엽기떡볶이', '02-1222-3345');
+
+insert into store
+values (2, '서울시 금천구 시흥동 42', '명량핫도그', '02-1452-1425');
+
+insert into store
+values (3, '서울시 금천구 독산동 43', '치킨플러스', '02-1885-8899');
+
+insert into store
+values (4, '서울시 금천구 시흥동 11', 'bhc', '02-1452-1425');
