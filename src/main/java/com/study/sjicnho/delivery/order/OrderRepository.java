75f642594ffae7ea09d601d3ccab514a587909c6
hyeji <hyeji6742@naver.com>
2024-05-02 17:26:20 +0900

    refactor:변경된 파일 제거

--- src/main/java/com/study/sjicnho/delivery/order/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.study.sjicnho.delivery.order;
-
-import org.aspectj.weaver.ast.Or;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
