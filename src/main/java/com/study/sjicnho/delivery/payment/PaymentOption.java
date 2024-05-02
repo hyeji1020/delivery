2064010151faa5a3c3551356a2b69c1e14c2e0b6
hyeji <hyeji6742@naver.com>
2024-05-02 17:17:45 +0900

    refactor:불필요한 import문 제거

--- src/main/java/com/study/sjicnho/delivery/payment/PaymentOption.java
+++ src/main/java/com/study/sjicnho/delivery/payment/PaymentOption.java
@@ -1,15 +1,5 @@
 package com.study.sjicnho.delivery.payment;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-
 public enum PaymentOption {
     //현금, 카드
     CASH, CARD
